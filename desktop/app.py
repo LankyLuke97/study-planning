8acf006eba47d4da460d6ef6c94028937b502b2e
LankyLuke97 <lukehowden@rocketmail.com>
2024-08-05 16:25:30 +0100

    Adds basic answer counter and test csv

--- desktop/app.py
+++ desktop/app.py
@@ -1,10 +1,46 @@
 from PyQt6.QtWidgets import *
-
+import pandas as pd
 import sys
 
 
 class AnswerTopicWindow(QMainWindow):
-    pass
+    def __init__(self, topic):
+        super().__init__()
+        self.setWindowTitle("Answer topic")
+        layout = QHBoxLayout()
+        self.answers = [0, 0]
+        self.topic = topic
+        correct_button = QPushButton("Correct")
+        incorrect_button = QPushButton("Incorrect")
+        finished_button = QPushButton("Finished")
+
+        correct_button.clicked.connect(self.correct_pushed)        
+        incorrect_button.clicked.connect(self.incorrect_pushed)        
+        finished_button.clicked.connect(self.finished_pushed)        
+
+        layout.addWidget(correct_button)
+        layout.addWidget(incorrect_button)
+        layout.addWidget(finished_button)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+        
+    def correct_pushed(self):
+        self.answers[0] += 1
+        print("adding 1 correct")
+        
+    def incorrect_pushed(self):
+        self.answers[1] += 1
+        print("adding 1 wrong")
+
+    def finished_pushed(self):
+        print("finished")
+        confidence = round(min(max((self.answers[0] / sum(self.answers)) - 0.4, 0.0), 0.55) / 0.55, 4)
+        print("Confidence: {}".format(confidence))
+        topics = pd.read_csv('topics.csv',index_col='topic_name')
+        topics.loc[self.topic, 'confidence'] = confidence
+        topics.to_csv('topics.csv')
 
 class ChooseTopicWindow(QMainWindow):
     def __init__(self):
@@ -36,7 +72,7 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 
-window = ChooseTopicWindow()
+window = AnswerTopicWindow('wills')
 window.show()
 
 app.exec()
