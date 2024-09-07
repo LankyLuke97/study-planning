d3fb5840947b3ce3c8bc9e75cee2d4ad13b6baaf
LankyLuke97 <lukehowden@rocketmail.com>
2024-09-07 20:32:06 +0100

    Adds topic name to answer window; adds correct/answered to list

--- desktop/app.py
+++ desktop/app.py
@@ -16,6 +16,9 @@ except ImportError:
 
 basedir = os.path.dirname(__file__)
 TOPICS = os.path.join(basedir, 'data', 'topics.csv')
+if not os.path.exists(TOPICS):
+    with open(TOPICS,'w') as data:
+        data.write("topic_name,confidence,review_date,last_correct,last_answered")
 
 def getReviewDate(confidence):
     max_day = 30
@@ -24,10 +27,12 @@ def getReviewDate(confidence):
 class AnswerTopicWindow(QMainWindow):
     def __init__(self, topic):
         super().__init__()
-        self.setWindowTitle("Answer topic")
-        layout = QGridLayout()
+        self.setWindowTitle(f"Answering {topic}")
+        layout = QVBoxLayout()
+        grid = QGridLayout()
         self.answers = [0, 0]
         self.topic = topic
+        topic_label = QLabel(topic)
         correct_button = QPushButton("Correct")
         incorrect_button = QPushButton("Incorrect")
         finished_button = QPushButton("Finished")
@@ -39,11 +44,15 @@ class AnswerTopicWindow(QMainWindow):
         self.correct_label = QLabel("Correct: 0")
         self.incorrect_label = QLabel("Incorrect: 0")
 
-        layout.addWidget(correct_button, 0, 0)
-        layout.addWidget(incorrect_button, 0, 1)
-        layout.addWidget(finished_button, 0, 2)
-        layout.addWidget(self.correct_label, 1, 0)
-        layout.addWidget(self.incorrect_label, 1, 1)
+        grid.addWidget(correct_button, 0, 0)
+        grid.addWidget(incorrect_button, 0, 1)
+        grid.addWidget(finished_button, 0, 2)
+        grid.addWidget(self.correct_label, 1, 0)
+        grid.addWidget(self.incorrect_label, 1, 1)
+        grid_widget = QWidget()
+        grid_widget.setLayout(grid)
+        layout.addWidget(topic_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(grid_widget)
 
         container = QWidget()
         container.setLayout(layout)
@@ -62,6 +71,8 @@ class AnswerTopicWindow(QMainWindow):
         topics = pd.read_csv(TOPICS,index_col='topic_name')
         topics.loc[self.topic, 'confidence'] = confidence
         topics.loc[self.topic, 'review_date'] = getReviewDate(confidence)
+        topics.loc[self.topic, 'last_correct'] = self.answers[0]
+        topics.loc[self.topic, 'last_answered'] = sum(self.answers)
         topics.to_csv(TOPICS)
         self.close()
 
@@ -107,16 +118,17 @@ class ChooseTopicWindow(QMainWindow):
         if not text.strip():
             QMessageBox.warning(self, "Input Error", "No topic entered")
         else:
-            pd.read_csv(TOPICS, index_col=False)._append({'topic_name': text.strip(), 'confidence': 0.0, 'review_date': getReviewDate(0.0)}, ignore_index=True).set_index('topic_name').to_csv(TOPICS)
+            pd.read_csv(TOPICS, index_col=False)._append({'topic_name': text.strip(), 'confidence': 0.0, 'review_date': getReviewDate(0.0), 'last_correct': 0.0, 'last_answered': 0.0}, ignore_index=True).set_index('topic_name').to_csv(TOPICS)
 
     def load_topics(self):
         df = pd.read_csv(TOPICS)
         df.review_date = pd.to_datetime(df.review_date, format='%Y-%m-%d')
-        return list(df.loc[df.review_date <= datetime.datetime.today(), 'topic_name'].sample(frac=1))
+        return [f"{row.topic_name} (Last attempt: {int(row.last_correct)}/{int(row.last_answered)})" for _, row in df.loc[df.review_date <= datetime.datetime.today(), ['topic_name','last_correct','last_answered']].sample(frac=1).iterrows()]
 
     def open_answer_topic(self, topic):
-        topic = self.widgetList.takeItem(self.widgetList.row(topic)) # Remove widget from list
-        self.window = AnswerTopicWindow(topic.text())
+        topic_label = self.widgetList.takeItem(self.widgetList.row(topic)) # Remove widget from list
+        topic_name = topic_label.text().split("(Last attempt: ")[0].strip()
+        self.window = AnswerTopicWindow(topic_name)
         self.window.show()
 
 app = QApplication(sys.argv)
